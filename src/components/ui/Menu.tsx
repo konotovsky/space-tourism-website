841fe5583057bf96ab5d431f24cc5ab34649c5f9
Vladyslav Konotovskyi <konotovsky@gmail.com>
2026-02-23 12:37:02 +0000

    Implement burger menu

--- src/components/ui/Menu.tsx
+++ src/components/ui/Menu.tsx
@@ -11,28 +11,62 @@ export default function Menu({ items }: MenuProps) {
 
   return (
     <>
-      <nav className="hidden gap-10 text-white uppercase sm:flex xl:bg-white/5 xl:pr-16 xl:pl-40 xl:backdrop-blur-[20px]">
-        {items.map((item, index) => {
-          const number = index.toString().padStart(2, "0");
+      <nav className="hidden text-white uppercase sm:flex xl:bg-white/5 xl:pr-16 xl:pl-40 xl:backdrop-blur-[20px]">
+        <ul className="flex gap-12">
+          {items.map((item, index) => {
+            const number = index.toString().padStart(2, "0");
 
-          return (
-            <NavLink
-              key={item.path}
-              to={item.path}
-              end={item.path === "/"}
-              className={({ isActive }) =>
-                `font-barlow-condensed text-base tracking-[2px] hover:border-white/50 sm:border-b-[3px] sm:py-[38.5px] ${isActive ? "sm:border-white hover:sm:border-white" : "sm:border-transparent"}`
-              }
-            >
-              <span className="mr-3 font-bold tracking-[2.7px]">{number}</span>
-              {item.name}
-            </NavLink>
-          );
-        })}
+            return (
+              <li className="sm:py-[38.5px]" key={item.path}>
+                <NavLink
+                  to={item.path}
+                  end={item.path === "/"}
+                  className={({ isActive }) =>
+                    `font-barlow-condensed text-base tracking-[2px] hover:border-white/50 sm:border-b-[3px] sm:py-[38.5px] ${isActive ? "sm:border-white hover:sm:border-white" : "sm:border-transparent"}`
+                  }
+                >
+                  <span className="mr-3 font-bold tracking-[2.7px]">
+                    {number}
+                  </span>
+                  {item.name}
+                </NavLink>
+              </li>
+            );
+          })}
+        </ul>
       </nav>
-      <button onClick={toggleMenu}>
+      <button className="z-1000 sm:hidden" onClick={toggleMenu}>
         <img src={isOpen ? menuClose : menuOpen} alt="Menu toggle" />
       </button>
+      {isOpen && (
+        <div className="absolute inset-0 ml-auto max-w-2/3 bg-white/5 backdrop-blur-[20px]">
+          <nav className="mt-33.25 text-white uppercase">
+            <ul className="flex flex-col gap-8">
+              {items.map((item, index) => {
+                const number = index.toString().padStart(2, "0");
+
+                return (
+                  <li key={item.path}>
+                    <NavLink
+                      to={item.path}
+                      end={item.path === "/"}
+                      onClick={() => setIsOpen(false)}
+                      className={({ isActive }) =>
+                        `font-barlow-condensed border-l-[3px] pl-8 text-base tracking-[2px] ${isActive ? "border-white" : "border-transparent"}`
+                      }
+                    >
+                      <span className="mr-3 font-bold tracking-[2.7px]">
+                        {number}
+                      </span>
+                      {item.name}
+                    </NavLink>
+                  </li>
+                );
+              })}
+            </ul>
+          </nav>
+        </div>
+      )}
     </>
   );
 }
